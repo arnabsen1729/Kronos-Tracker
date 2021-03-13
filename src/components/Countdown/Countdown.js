7800970397073781c57c583a64f30f794b9eeea4
Krishnendu Bera <bera.krishnendu36@gmail.com>
2021-03-13 12:35:53 +0530

    UPD: Countdown added

---
+++ src/components/Countdown/Countdown.js
@@ -0,0 +1,146 @@
+
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './Countdown.css';
+import { faCheckCircle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+//import video from './video.mp4';
+
+function Countdown() {
+
+
+    const FULL_DASH_ARRAY = 283;
+    const WARNING_THRESHOLD = 10;
+    const ALERT_THRESHOLD = 5;
+
+    const COLOR_CODES = {
+        info: {
+            color: "green"
+        },
+        warning: {
+            color: "orange",
+            threshold: WARNING_THRESHOLD
+        },
+        alert: {
+            color: "red",
+            threshold: ALERT_THRESHOLD
+        }
+    };
+    const TIME_LIMIT = 20;
+    let timePassed = 0;
+    let timeLeft = TIME_LIMIT;
+    let timerInterval = null;
+    let remainingPathColor = COLOR_CODES.info.color;
+
+    function startTimer() {
+        timerInterval = setInterval(() => {
+            timePassed = timePassed += 1;
+            timeLeft = TIME_LIMIT - timePassed;
+            document.getElementById("base-timer-label").innerHTML = formatTime(
+                timeLeft
+            );
+            setCircleDasharray();
+            setRemainingPathColor(timeLeft);
+
+            if (timeLeft === 0) {
+                onTimesUp();
+            }
+        }, 1000);
+    }
+
+    useEffect(() => {
+        startTimer();
+    }, [])
+
+    function onTimesUp() {
+        clearInterval(timerInterval);
+    }
+
+    function giveUp() {
+        onTimesUp();
+    }
+    function done() {
+        onTimesUp();
+    }
+
+    function formatTime(time) {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+
+        return `${minutes}:${seconds}`;
+    }
+
+    function setRemainingPathColor(timeLeft) {
+        const { alert, warning, info } = COLOR_CODES;
+        if (timeLeft <= alert.threshold) {
+            document
+                .getElementById("base-timer-path-remaining")
+                .classList.remove(warning.color);
+            document
+                .getElementById("base-timer-path-remaining")
+                .classList.add(alert.color);
+        } else if (timeLeft <= warning.threshold) {
+            document
+                .getElementById("base-timer-path-remaining")
+                .classList.remove(info.color);
+            document
+                .getElementById("base-timer-path-remaining")
+                .classList.add(warning.color);
+        }
+    }
+
+    function calculateTimeFraction() {
+        const rawTimeFraction = timeLeft / TIME_LIMIT;
+        return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    }
+
+    function setCircleDasharray() {
+        const circleDasharray = `${(
+            calculateTimeFraction() * FULL_DASH_ARRAY
+        ).toFixed(0)} 283`;
+        document
+            .getElementById("base-timer-path-remaining")
+            .setAttribute("stroke-dasharray", circleDasharray);
+    }
+
+
+    return (
+
+        <div className="countdown-wrap h-screen">
+            {/* <video loop autoPlay style={{ position: "absolute", height: "100%", width: "100%", objectFit: "cover" }}>
+                <source src={process.env.PUBLIC_URL + "/assets/video.mp4"} type="video/mp4" />
+            </video> */}
+            <audio loop autoplay><source src={process.env.PUBLIC_URL + "/assets/rain.mp3"} type="audio/mp3" /></audio>
+            <div id="app" className="countdown-app border border-red-700">
+                <div class="base-timer ">
+                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                        <g class="base-timer__circle">
+                            <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                            <path
+                                id="base-timer-path-remaining"
+                                stroke-dasharray="283"
+                                class={`base-timer__path-remaining ${remainingPathColor}`}
+                                d="
+                                M 50, 50
+                                m -45, 0
+                                a 45,45 0 1,0 90,0
+                                a 45,45 0 1,0 -90,0"
+                            ></path>
+                        </g>
+                    </svg>
+                    <span id="base-timer-label" class="base-timer__label">{formatTime(timeLeft)}</span>
+                </div>
+            </div>
+            <div className="countdown-button-wrap">
+                <div onClick={done} className="countdown-button hover:bg-gray-500 cursor-pointer"><FontAwesomeIcon icon={faCheckCircle} size="2x" className="mr-2" />Done</div>
+                <div onClick={giveUp} className="countdown-button hover:bg-gray-500 cursor-pointer"><FontAwesomeIcon icon={faExclamationCircle} size="2x" className="mr-2" /> Give Up</div>
+            </div>
+        </div>
+    )
+}
+
+export default Countdown
